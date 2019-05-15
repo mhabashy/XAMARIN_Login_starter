73e4ff2222034ef8651324fa740b9bc2729011ee
Michael Habashy <mhabashy@mail.usf.edu>
2019-05-14 22:54:36 -0400

    project files

---
+++ smsmapp/smsmapp/Default.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace smsmapp
+{
+    public partial class Default : ContentPage
+    {
+        private HttpClient client = new HttpClient();
+
+        public Default()
+        {
+            InitializeComponent();
+        }
+
+        public class Checks
+        {
+            public int contact
+            {
+                get;
+                set;
+            }
+
+            public int deacon
+            {
+                get;
+                set;
+            }
+
+            public int newsletter
+            {
+                get;
+                set;
+            }
+        }
+
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            // myValue.Text = getEmail.Text;
+            // Get resources'
+
+            var content = await client.GetStringAsync("http://[URL HERE]/checkemail.php?email=" + getEmail.Text);
+            var posts = JsonConvert.DeserializeObject<Checks>(content);
+            myValue.Text = "Contact " + posts.contact + ", Deacon " + posts.deacon + ", Newletter " + posts.newsletter;
+
+        }
+    }
+}
